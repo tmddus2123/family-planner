975ded4345cd812da2f63a278f2ec524a11baabe
DICONLSY\user <tmddus2123@gmail.com>
2026-04-02 14:43:07 +0900

    fix

--- js/config/kakao-config.js
+++ js/config/kakao-config.js
@@ -1,10 +1,7 @@
 window.KAKAO_CONFIG = {
   javascriptKey: "ca9b65d724311438d7a6a0bb5cde8bde",
   scopes: [
-    "profile_nickname",
-    "profile_image",
     "account_email",
-    "talk_message",
   ],
   apiBaseUrl: "https://kapi.kakao.com",
 };
